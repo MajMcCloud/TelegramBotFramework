eebb5cb6f560388559d248f3a011bc680b3aba88
FlorianDahn <ich@florian-dahn.de>
2020-04-21 21:44:01 +0200

    Create eSettings.cs
    - adding eSettings enumeration

---
+++ TelegramBotBase/Enums/eSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramBotBase.Enums
+{
+    public enum eSettings
+    {
+        /// <summary>
+        /// How often could a form navigate to another (within one user action/call/message)
+        /// </summary>
+        NavigationMaximum = 1,
+
+
+        /// <summary>
+        /// Loggs all messages and sent them to the event handler
+        /// </summary>
+        LogAllMessages = 2,
+
+
+        /// <summary>
+        /// Skips all messages during running (good for big delay updates)
+        /// </summary>
+        SkipAllMessages = 3,
+
+
+
+
+
+    }
+}
