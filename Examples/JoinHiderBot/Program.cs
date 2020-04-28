7d8895850ad350e74995bd15bca3e10569d55fa6
FlorianDahn <ich@florian-dahn.de>
2020-04-28 20:10:18 +0200

    Example for a Join/Leave Messages delete Bot

---
+++ Examples/JoinHiderBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace JoinHiderBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String apiKey = "";
+
+            var bot = new TelegramBotBase.BotBase<forms.Start>(apiKey);
+
+            bot.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
