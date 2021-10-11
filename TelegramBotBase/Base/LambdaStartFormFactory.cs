f3167eda2c9d4adaae0513416d8a046a9f520a38
Victor <victorrar.ru@gmail.com>
2021-10-11 14:37:15 +0300

    Add LambdaStartFormFactory.cs that allows pass parameters without creating factory class
    eg. BotBase bb = new BotBase(api, new LambdaStartFormFactory(
        () => new Start(posible, parameters)
    ));

---
+++ TelegramBotBase/Base/LambdaStartFormFactory.cs
@@ -0,0 +1,22 @@
+﻿using TelegramBotBase.Form;
+using TelegramBotBase.Interfaces;
+
+namespace TelegramBotBase.Base
+{
+    public class LambdaStartFormFactory : IStartFormFactory
+    {
+        public delegate FormBase CreateFormDelegate();
+
+        private readonly CreateFormDelegate _lambda;
+
+        public LambdaStartFormFactory(CreateFormDelegate lambda)
+        {
+            _lambda = lambda;
+        }
+
+        public FormBase CreateForm()
+        {
+            return _lambda();
+        }
+    }
+}
