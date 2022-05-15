3386db0f66b3a04becd0ca105bc7382aea8cb059
FlorianDahn <ich@florian-dahn.de>
2022-05-15 23:23:29 +0200

    Updating Test project
    - adding "myid" command to get own device id

--- TelegramBotBase.Test/Program.cs
+++ TelegramBotBase.Test/Program.cs
@@ -28,6 +28,7 @@ namespace TelegramBotBaseTest
                       .CustomCommands(a =>
                       {
                           a.Start("Starts the bot");
+                          a.Add("myid", "Returns my Device ID");
                           a.Help("Should show you some help");
                           a.Settings("Should show you some settings");
                           a.Add("form1", "Opens test form 1");
@@ -39,45 +40,7 @@ namespace TelegramBotBaseTest
                       .Build();
 
 
-            bb.BotCommand += async (s, en) =>
-            {
-                switch (en.Command)
-                {
-                    case "/start":
-
-                        var start = new Menu();
-
-                        await en.Device.ActiveForm.NavigateTo(start);
-
-                        break;
-                    case "/form1":
-
-                        var form1 = new TestForm();
-
-                        await en.Device.ActiveForm.NavigateTo(form1);
-
-                        break;
-
-                    case "/form2":
-
-                        var form2 = new TestForm2();
-
-                        await en.Device.ActiveForm.NavigateTo(form2);
-
-                        break;
-
-                    case "/params":
-
-                        String m = en.Parameters.DefaultIfEmpty("").Aggregate((a, b) => a + " and " + b);
-
-                        await en.Device.Send("Your parameters are: " + m, replyTo: en.Device.LastMessageId);
-
-                        en.Handled = true;
-
-                        break;
-                }
-
-            };
+            bb.BotCommand += Bb_BotCommand;
 
             //Update Bot commands to botfather
             bb.UploadBotCommands().Wait();
@@ -91,8 +54,6 @@ namespace TelegramBotBaseTest
 
             bb.Start();
 
-
-
             Console.WriteLine("Telegram Bot started...");
 
             Console.WriteLine("Press q to quit application.");
@@ -104,6 +65,54 @@ namespace TelegramBotBaseTest
 
         }
 
+        private static async Task Bb_BotCommand(object sender, TelegramBotBase.Args.BotCommandEventArgs en)
+        {
+            switch (en.Command)
+            {
+                case "/start":
 
+
+                    var start = new Menu();
+
+                    await en.Device.ActiveForm.NavigateTo(start);
+
+                    break;
+                case "/form1":
+
+                    var form1 = new TestForm();
+
+                    await en.Device.ActiveForm.NavigateTo(form1);
+
+
+                    break;
+
+
+                case "/form2":
+
+                    var form2 = new TestForm2();
+
+                    await en.Device.ActiveForm.NavigateTo(form2);
+
+                    break;
+
+                case "/myid":
+
+                    await en.Device.Send($"Your Device ID is: {en.DeviceId}");
+
+                    en.Handled = true;
+
+                    break;
+
+                case "/params":
+
+                    String m = en.Parameters.DefaultIfEmpty("").Aggregate((a, b) => a + " and " + b);
+
+                    await en.Device.Send("Your parameters are: " + m, replyTo: en.Device.LastMessageId);
+
+                    en.Handled = true;
+
+                    break;
+            }
+        }
     }
 }
