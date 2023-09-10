d21a5f8937e4972899c6c10dc6f25db476aff541
Florian Zevedei <florian.dahn@fdsoftware.de>
2023-09-10 18:03:33 +0200

    Removing Telegram.Bot.Extensions.Polling nuget package

--- TelegramBotBase/Base/MessageClient.cs
+++ TelegramBotBase/Base/MessageClient.cs
@@ -7,7 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
-using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 
 namespace TelegramBotBase.Base;
