727d9d5508272d0fbcb7f3bd12acd199fbf048c1
Florian Zevedei <florian.dahn@fdsoftware.de>
2023-09-12 14:15:25 +0200

    Updating project file and version number

--- TelegramBotBase/Properties/AssemblyInfo.cs
+++ TelegramBotBase/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
