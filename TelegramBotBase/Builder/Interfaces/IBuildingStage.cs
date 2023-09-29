e6a48115a6c2f5667a8d550c03dfa893665c988d
Florian Zevedei <florian.dahn@fdsoftware.de>
2023-09-29 18:28:55 +0200

    Adding xml comments to BotBase

--- TelegramBotBase/Builder/Interfaces/IBuildingStage.cs
+++ TelegramBotBase/Builder/Interfaces/IBuildingStage.cs
@@ -2,5 +2,9 @@
 
 public interface IBuildingStage
 {
+    /// <summary>
+    /// Creates a full BotBase instance with all parameters previously set.
+    /// </summary>
+    /// <returns></returns>
     BotBase Build();
 }
