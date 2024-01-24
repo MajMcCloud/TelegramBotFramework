d910b42473106f24424aaa6999f072907a2fece6
Florian Zevedei <florian.dahn@fdsoftware.de>
2024-01-24 22:20:37 +0100

    Fix

--- Experiments/ExternalActionManager/DemoBot/ActionManager/ExternalActionManager.cs
+++ Experiments/ExternalActionManager/DemoBot/ActionManager/ExternalActionManager.cs
@@ -20,7 +20,7 @@ namespace DemoBot.ActionManager
                 if (!action.DoesFit(mr.RawData))
                     continue;
 
-                await action.DoAction(mr.RawData, ur, mr);
+                await action.DoAction(ur, mr);
 
                 return true;
             }
