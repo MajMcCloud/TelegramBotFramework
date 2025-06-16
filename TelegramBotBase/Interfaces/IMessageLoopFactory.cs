ed8a436b9915e2927197bf1a8aa114c2420b2086
Florian Zevedei <florian.dahn@fdsoftware.de>
2025-06-16 15:05:11 +0200

    Adding comments

--- TelegramBotBase/Interfaces/IMessageLoopFactory.cs
+++ TelegramBotBase/Interfaces/IMessageLoopFactory.cs
@@ -8,6 +8,14 @@ namespace TelegramBotBase.Interfaces;
 
 public interface IMessageLoopFactory
 {
+    /// <summary>
+    /// Processes incoming messages and updates within the bot's message loop.
+    /// </summary>
+    /// <param name="bot">The bot instance responsible for handling the message loop.</param>
+    /// <param name="session">The device session associated with the current user or client.</param>
+    /// <param name="ur">The update result containing information about the latest updates or events.</param>
+    /// <param name="e">The message result representing the incoming message or event to be processed.</param>
+    /// <returns>A task that represents the asynchronous operation of the message loop.</returns>
     Task MessageLoop(BotBase bot, IDeviceSession session, UpdateResult ur, MessageResult e);
 
 
