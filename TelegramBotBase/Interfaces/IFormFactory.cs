ba60fe7c91310bf71d4733be3f6cb8621b409356
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-05 00:55:31 +0300

    Extend IFormFactory with generic and type-based creation
    Added CreateForm(Type formType) and CreateForm<T>() methods to the IFormFactory interface to support more flexible form instantiation.

--- TelegramBotBase/Interfaces/IFormFactory.cs
+++ TelegramBotBase/Interfaces/IFormFactory.cs
@@ -1,8 +1,11 @@
-﻿using TelegramBotBase.Form;
+﻿using System;
+using TelegramBotBase.Form;
 
 namespace TelegramBotBase.Interfaces;
 
 public interface IFormFactory
 {
     FormBase CreateStartForm();
+    FormBase CreateForm(Type formType);
+    FormBase CreateForm<T>() where T : FormBase, new();
 }
