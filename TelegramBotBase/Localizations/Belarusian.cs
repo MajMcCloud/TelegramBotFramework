dac2f243bded41cbe55f59b04bebc5bf15a2c7df
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-08 19:04:18 +0300

    Add belarusian localization class

---
+++ TelegramBotBase/Localizations/Belarusian.cs
@@ -0,0 +1,39 @@
+﻿namespace TelegramBotBase.Localizations;
+
+public sealed class Belarusian : Localization
+{
+    public Belarusian()
+    {
+        Values["Language"] = "Беларуская (Belarusian)";
+        Values["ButtonGrid_Title"] = "Меню";
+        Values["ButtonGrid_NoItems"] = "Элементаў няма.";
+        Values["ButtonGrid_PreviousPage"] = "◀️";
+        Values["ButtonGrid_NextPage"] = "▶️";
+        Values["ButtonGrid_CurrentPage"] = "Старонка {0} з {1}";
+        Values["ButtonGrid_SearchFeature"] = "💡 Напішыце паведамленне для фільтра. Націсніце 🔍, каб скінуць фільтр.";
+        Values["ButtonGrid_Back"] = "Назад";
+        Values["TaggedButtonGrid_TotalTags"] = "Усяго: {0}";
+        Values["TaggedButtonGrid_CheckedTags"] = "Выбрана: {0}";
+        Values["TaggedButtonGrid_CheckAll"] = "Адзначыць усё";
+        Values["TaggedButtonGrid_UncheckAll"] = "Зняць усё";
+        Values["CalendarPicker_Title"] = "Выберыце дату";
+        Values["CalendarPicker_PreviousPage"] = "◀️";
+        Values["CalendarPicker_NextPage"] = "▶️";
+        Values["TreeView_Title"] = "Выберыце вузел";
+        Values["TreeView_LevelUp"] = "🔼 уверх";
+        Values["ToggleButton_On"] = "Укл";
+        Values["ToggleButton_Off"] = "Выкл";
+        Values["ToggleButton_OnIcon"] = "⚫";
+        Values["ToggleButton_OffIcon"] = "⚪";
+        Values["ToggleButton_Title"] = "Выбраць";
+        Values["ToggleButton_Changed"] = "Абрана";
+        Values["MultiToggleButton_SelectedIcon"] = "✅";
+        Values["MultiToggleButton_Title"] = "Множны выбар";
+        Values["MultiToggleButton_Changed"] = "Абрана";
+        Values["PromptDialog_Back"] = "Назад";
+        Values["ToggleButton_Changed"] = "Налада зменена";
+        Values["ButtonGrid_SearchIcon"] = "🔍";
+        Values["TaggedButtonGrid_TagIcon"] = "📁";
+        Values["Label_Text"] = "Тэкст па змаўчанні";
+    }
+}
