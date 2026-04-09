d64f4ed9811bb28ee9a8d102ca01c92094f75209
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-10 01:54:37 +0300

    Create FormDiEscort sealed class with public "ServiceScope" and "FormFactory" fields

---
+++ TelegramBotBase/DependencyInjection/FormDiEscort.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TelegramBotBase.Interfaces;
+
+namespace TelegramBotBase.DependencyInjection;
+
+public sealed class FormDiEscort
+{
+    public IServiceScope ServiceScope;
+    public IFormFactory FormFactory;
+}
