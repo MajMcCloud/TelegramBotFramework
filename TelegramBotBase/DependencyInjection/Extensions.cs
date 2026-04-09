a59a8005c32d2551bdd17230753f66382009524b
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-10 02:00:19 +0300

    Delete useless usings

--- TelegramBotBase/DependencyInjection/Extensions.cs
+++ TelegramBotBase/DependencyInjection/Extensions.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 using TelegramBotBase.Form;
 
