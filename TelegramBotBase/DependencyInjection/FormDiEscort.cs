4a9d31d66c678326fdf4e9f0c6c5b3769fc37434
Usevalad Buben <usevaladbuben@gmail.com>
2026-04-10 03:01:32 +0300

    Implement IAsyncDisposable into FormDiEscort, define "DisposeAsync" method

--- TelegramBotBase/DependencyInjection/FormDiEscort.cs
+++ TelegramBotBase/DependencyInjection/FormDiEscort.cs
@@ -1,10 +1,30 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using TelegramBotBase.Interfaces;
 
 namespace TelegramBotBase.DependencyInjection;
 
-public sealed class FormDiEscort
+public sealed class FormDiEscort : IAsyncDisposable
 {
     public IServiceScope ServiceScope;
     public IFormFactory FormFactory;
+    
+    public async ValueTask DisposeAsync()
+    {
+        FormFactory = null;
+
+        if (ServiceScope == null) return;
+
+        if (ServiceScope is IAsyncDisposable ad)
+        {
+            await ad.DisposeAsync();
+        }
+        else
+        {
+            ServiceScope.Dispose();
+        }
+        
+        ServiceScope = null;
+    }
 }
