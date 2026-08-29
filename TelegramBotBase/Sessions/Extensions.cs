073a79a80a2468b1d736cb43e64cf430be80409c
Florian Zevedei <florian.dahn@fdsoftware.de>
2026-08-29 17:53:22 +0200

    Adding cancellation token parameter to Dispatch extensions & update DeviceSession parameter

--- TelegramBotBase/Sessions/Extensions.cs
+++ TelegramBotBase/Sessions/Extensions.cs
@@ -1,18 +1,20 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
+using TelegramBotBase.Interfaces;
 
 namespace TelegramBotBase.Sessions;
 
 public static class Extensions
 {
-    public static Task Dispatch(this DeviceSession ds, Func<ITelegramBotClient, Task> request)
+    public static Task Dispatch(this IDeviceSession ds, Func<ITelegramBotClient, Task> request, CancellationToken ct = default )
     {
-        return ds.Dispatcher.Dispatch(ds, request);
+        return ds.Dispatcher.Dispatch(ds, request, ct);
     }
 
-    public static Task<T> Dispatch<T>(this DeviceSession ds, Func<ITelegramBotClient, Task<T>> request)
+    public static Task<T> Dispatch<T>(this IDeviceSession ds, Func<ITelegramBotClient, Task<T>> request, CancellationToken ct = default)
     {
-        return ds.Dispatcher.Dispatch(ds, request);
+        return ds.Dispatcher.Dispatch(ds, request, ct);
     }
 }
